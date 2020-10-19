f72bed834f78e459227f57e9d96ed0e099669460
Udayraj Mandke <urmandke@gmail.com>
2020-10-19 13:07:34 +0530

    Middleware added to app (express)

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+
+const app = express(); //not sure what this means --
+
+var corsOption = {
+    origin: "http://localhost:0001"
+}
+
+app.use(cors(corsOptions));
+
+//parse request of content-type, application/json
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended:true}));
+
