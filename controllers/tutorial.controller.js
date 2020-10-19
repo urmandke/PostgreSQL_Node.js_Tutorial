40d3eb973db768c8a8f91227151aa709fbef8e28
Udayraj Mandke <urmandke@gmail.com>
2020-10-19 14:11:12 +0530

    create tutorial controller created

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -4,9 +4,31 @@ const Op = db.Sequelize.Op;
 
 //Create and Save a new Tutorial
 exports.create = (req,res) => {
+    //Validate request
+    if(!req.body.title){
+        res.status(400).send({message:"Content can not be empty!"});
+        return;
+    };
 
-};
+    //Create a tutorial object from request body
+    const tutorial = {
+        title:req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published: false
+    };
 
+    //Save Tutorial in the database
+    Tutorial.create(tutorial)
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occured while creating the Tutorial."
+        });
+    });
+}
 //Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
 
