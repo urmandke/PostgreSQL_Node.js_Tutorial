2a8ba14660450ba350d91074ad27bd4a60460087
Udayraj Mandke <urmandke@gmail.com>
2020-10-19 13:25:24 +0530

    Sequelize config added to index.js

--- config/models/index.js
+++ config/models/index.js
@@ -1,1 +1,14 @@
 const dbConfig = require('../config/db.config.js');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(db.Config.DB, dbConfig.User, dbConfig.Password,{
+    host: dbConfig.HOST,
+    dailect: dbConfig.pool.min,
+    aquire: dbConfig.pool.aquire,
+    idle: dbConfig.pool.idle,
+})
+
+const db = {};
+
+db.Sequelize = Sequlize;
+db.sequelize = sequelize;
